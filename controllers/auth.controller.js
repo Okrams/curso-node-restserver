4609cf0626299c7481734ce0d98a301a691b6f37
Eduardo Pérez Ramírez <ed.pe.r@hotmail.com>
2021-03-17 19:44:20 -0600

    GoogleSign added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,6 +3,7 @@ const { response, request } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req, res) => {
 
@@ -50,6 +51,51 @@ const login = async(req, res) => {
     }
 }
 
+const googleSignin = async(req = request, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        const {nombre, correo, img} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne( {correo} );
+        if( !usuario ){
+            // tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        // Si el usuario en BD
+        if( !usuario.estado ){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+        
+        res.json({
+            usuario,
+            token
+        }); 
+
+    } catch (error) {
+        res.status(400).json({
+            msg:'Token de Google no válido'
+        })
+    }
+
+}
+
 module.exports = {
     login,
+    googleSignin
 }
